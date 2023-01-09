a6be9cff18c5370ee47e67f895570bbec2af2554
hocheung-chromium <hocheung@chromium.org>
2023-01-09 19:06:26 +0800

    New Features
    New Features

---
+++ chrome/browser/yfqh_flag_choices.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Cheung_yfqh. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_YFQH_FLAG_CHOICES_H_
+#define CHROME_BROWSER_YFQH_FLAG_CHOICES_H_
+
+const FeatureEntry::Choice kCloseConfirmation[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {"Show confirmation with last window",
+     "close-confirmation",
+     "last"},
+    {"Show confirmation with multiple windows",
+     "close-confirmation",
+     "multiple"},
+};
+
+const FeatureEntry::Choice kCloseWindowWithLastTab[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {"Never",
+     "close-window-with-last-tab",
+     "never"},
+};
+
+#endif  // CHROME_BROWSER_YFQH_FLAG_CHOICES_H_
