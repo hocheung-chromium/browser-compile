36f8e757a7132b28a104c1516a9870b8b7c860f1
hocheung-chromium <hocheung@chromium.org>
2023-02-07 15:40:33 +0800

    update

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7862,6 +7862,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kFedCmAutoReauthnDescription, kOsAll,
      FEATURE_VALUE_TYPE(features::kFedCmAutoReauthn)},
 
+    {"fedcm-idp-registration", flag_descriptions::kFedCmIdPRegistrationName,
+     flag_descriptions::kFedCmIdPRegistrationDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(features::kFedCmIdPRegistration)},
+
     {"fedcm-iframe-support", flag_descriptions::kFedCmIframeSupportName,
      flag_descriptions::kFedCmIframeSupportDescription, kOsAll,
      FEATURE_VALUE_TYPE(features::kFedCmIframeSupport)},
